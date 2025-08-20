4ae5f242091f43b76ee51831ec5ca01e1d4651f6
sharksandcats <mayaosam1@gmail.com>
2025-08-20 22:27:46 +0300

    component: CreatePost

---
+++ src/Pages/Create.jsx
@@ -0,0 +1,39 @@
+import {ArrowLeft, SendHorizontal, MapPinPlus, Phone, ImageUp } from 'lucide-react';
+
+const Create = () =>{
+    return(
+        <div className='create-container'>
+            <div className='create-header'>
+                <ArrowLeft className='nav-icon'/>
+                <h2> New Post </h2>
+                <SendHorizontal className='nav-icon'/>
+            </div>
+
+            <hr/>
+
+            <div className="upload-box">
+                <ImageUp size={48} />
+            </div>
+
+            <textarea
+                className="caption-input"
+                placeholder="Add a caption..."
+                rows="3"
+            />
+
+            <hr/>
+
+            <div className='options'>
+                <p> Add Location </p>
+                <MapPinPlus size={18}/>
+            </div>
+
+            <div className='options'>
+                <p>Contact Info</p>
+                <Phone size={18}/>
+            </div>
+        </div>
+    )
+}
+
+export default Create;
