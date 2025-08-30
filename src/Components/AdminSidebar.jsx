b4c5472b4af5a16acdf648b19e5746dba1e7f3dd
sharksandcats <mayaosam1@gmail.com>
2025-08-30 19:22:11 +0300

    feat: responsiveness on mobiles and tablets done and ready

--- src/Components/AdminSidebar.jsx
+++ src/Components/AdminSidebar.jsx
@@ -1,26 +1,43 @@
-import { House, CircleUserRound, LogOut} from "lucide-react";
+import { House, CircleUserRound, LogOut, Menu } from "lucide-react";
 import {BrowserRouter as Router, Routes, Route, Link, NavLink} from "react-router-dom";
-
+import { useState } from "react";
 
 import "../css/Sidebar.css"
 
 //NavLink instead of Link so i can add hover and active css
 function AdminSidebar(){
+
+    const [isOpen, setIsOpen] = useState(false)
+    const handleOverlayClick = () => setIsOpen(false);
+
     return(
-        <aside>
+        <>
+            <div className="hamburger"
+             onClick={() => setIsOpen(!isOpen)}>
+                <Menu size={28} />
+        </div>
+
+        <div
+            className={`overlay ${isOpen ? "active" : ""}`}
+            onClick={handleOverlayClick}>
+        </div>
+
+        <aside className={isOpen ? "active" : ""}>
             <div>
                 <h1> Mubadalah </h1>
                 <nav>
                     <NavLink to="/admin"
                         className={({ isActive }) => (isActive ? "active" : "")}
+                        onClick={() => setIsOpen(false)}
                         end
                     >
                         <House /> Home </NavLink> 
-                    <NavLink to="/admin/Maya"><CircleUserRound /> Profile </NavLink> 
+                    <NavLink to="/admin/Maya" onClick={() => setIsOpen(false)}><CircleUserRound /> Profile </NavLink> 
                 </nav>
             </div>
-            <button className="logout"><LogOut /> Logout </button>
+            <button className="logout" onClick={() => setIsOpen(false)}><LogOut /> Logout </button>
         </aside>
+        </>
     )
 }
 
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,24 +1,41 @@
-import { House, SquarePlus, CircleUserRound, LogOut, LogIn } from "lucide-react";
+import { House, SquarePlus, CircleUserRound, LogOut, LogIn, Menu } from "lucide-react";
 import {BrowserRouter as Router, Routes, Route, Link, NavLink} from "react-router-dom";
 
 
 import "../css/Sidebar.css"
+import { useState } from "react";
 
 //NavLink instead of Link so i can add hover and active css
 function Sidebar(){
+
+    const [isOpen, setIsOpen] = useState(false)
+    const handleOverlayClick = () => setIsOpen(false);
+
     return(
-        <aside>
+    <>
+        <div className="hamburger"
+             onClick={() => setIsOpen(!isOpen)}>
+                <Menu size={28} />
+        </div>
+
+        <div
+            className={`overlay ${isOpen ? "active" : ""}`}
+            onClick={handleOverlayClick}>
+        </div>
+
+        <aside className={isOpen ? "active" : ""}>
             <div>
                 <h1> Mubadalah </h1>
                 <nav>
-                    <NavLink to="/"><House /> Home </NavLink> 
-                    <NavLink to="/create"><SquarePlus /> Create </NavLink>
-                    <NavLink to="/profile/Asem"><CircleUserRound /> Profile </NavLink> 
-                    <NavLink to="/login" className="login"><LogIn /> Login </NavLink>
+                    <NavLink to="/" onClick={() => setIsOpen(false)}><House /> Home </NavLink> 
+                    <NavLink to="/create" onClick={() => setIsOpen(false)}><SquarePlus /> Create </NavLink>
+                    <NavLink to="/profile/Asem" onClick={() => setIsOpen(false)}><CircleUserRound /> Profile </NavLink> 
+                    <NavLink to="/login" className="login" onClick={() => setIsOpen(false)}><LogIn /> Login </NavLink>
                 </nav>
             </div>
-            <button className="logout"><LogOut /> Logout </button>
+            <button className="logout" onClick={() => setIsOpen(false)}><LogOut /> Logout </button>
         </aside>
+    </>
     )
 }
 
